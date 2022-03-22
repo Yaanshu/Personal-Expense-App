587e306ba97b99b3db709c3670e36d1776a1ff1d
Yanshu Gautam <gautamyanshu345@gmail.com>
2022-03-22 23:48:57 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,36 +4,46 @@ const headingElement = document.querySelector("#headingtotal")
 // get the reference to desc element
 const inputDescElement = document.querySelector("#inputDesc")
 
+//ref to input amount
+const inputElement = document.querySelector("#inputAmount");
+
 // init value of expense at 0
 let totalExpense = 0;
 
 //set the heading element to TotalExpense
 headingElement.textContent = totalExpense; 
 
+//allExpenses at one place
+const allExpenses = [];
+
 //onButtonClick add InputAmount to TotalExpense
 function addExpenseToTotal() {
+    const expenseItem = {};
 
     //read value from InputAmount
-    const inputElement = document.querySelector("#inputAmount");
     const textAmount = inputElement.value;
-    console.log({textAmount});
+
+    //read the desc from inputDesc
+    const textDesc = inputDescElement.value;
+    // console.log({textDesc, textAmount})
 
     //convert it to number
     const expense = parseInt(textAmount, 10);
-    console.log({expense});
-    
+
+    //put it in object
+    expenseItem.desc = textDesc;
+    expenseItem.textAmount = expense;
+
+    allExpenses.push(expenseItem);
+
     //add that value to totalExpense
     totalExpense = totalExpense + expense;
-    console.log({totalExpense});
 
     // set the heading element to totalExpense
     headingElement.textContent = totalExpense; 
 }
     
-// read value from InputAmount
-
-
-
+// get the btn element
 const element = document.querySelector("#btnAddExpense");
 
 // Listen to click event
@@ -41,7 +51,6 @@ element.addEventListener("click", addExpenseToTotal, false);
 
 //Take a number which starts from zero
 let counter = 0;
-
 
 //Function to increment the number
 
